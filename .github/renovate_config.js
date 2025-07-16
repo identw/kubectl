a88b7655dd3a9e918b531cb0e9df37e8b4f38d6a
Dmitry Sergeev <identw@gmail.com>
2025-07-17 00:34:54 +0400

    WIP

--- .github/renovate_config.js
+++ .github/renovate_config.js
@@ -1,22 +1,4 @@
 module.exports = {
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
   platform: 'github',
-  forkProcessing: 'enabled',
   repositories: ['identw/kubectl'],
-  packageRules: [
-    {
-      description: 'lockFileMaintenance',
-      matchUpdateTypes: [
-        'pin',
-        'digest',
-        'patch',
-        'minor',
-        'major',
-        'lockFileMaintenance',
-      ],
-      dependencyDashboardApproval: false,
-      minimumReleaseAge: '0 days',
-    },
-  ],
 };
